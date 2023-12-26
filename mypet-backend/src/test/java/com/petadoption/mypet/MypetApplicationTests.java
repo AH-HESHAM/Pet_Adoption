ce5b2f797068cf467ab686bf46802dc3b9ac23c7
MohAmin22 <alsayedm12@gmail.com>
2023-12-26 14:18:48 +0200

    back end init

---
+++ mypet-backend/src/test/java/com/petadoption/mypet/MypetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petadoption.mypet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
