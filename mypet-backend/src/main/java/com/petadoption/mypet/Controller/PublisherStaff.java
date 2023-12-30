ed64a172becdc83297553f05a8c3946417390f18
Ahmed Hesham <96313662+AH-HESHAM@users.noreply.github.com>
2023-12-30 08:47:26 +0200

    a

--- mypet-backend/src/main/java/com/petadoption/mypet/Controller/PublisherStaff.java
+++ mypet-backend/src/main/java/com/petadoption/mypet/Controller/PublisherStaff.java
@@ -1,11 +1,13 @@
 package com.petadoption.mypet.Controller;
 
 
+import com.petadoption.mypet.DTO.ManagerPostDTO;
 import com.petadoption.mypet.DTO.PetDTO;
 import com.petadoption.mypet.Service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,12 @@ public class PublisherStaff {
     @Autowired
     private PetService petService;
     @PostMapping("/pets")
-    List<PetDTO> getPets(@RequestParam("staffId") int staffId) {
-        return petService.getAllPetsForStaff(staffId);
+    List<PetDTO> getPets(@RequestBody ManagerPostDTO staffId) {
+        try {
+            return petService.getAllPetsForStaff(staffId.getManagerId());
+        } catch (Exception e) {
+            return List.of();
+        }
+        
     }
 }
--- pet-adoption-frontend/src/SiteRoles/Staff/Service/StaffService.js
+++ pet-adoption-frontend/src/SiteRoles/Staff/Service/StaffService.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import {serverHost} from "../../../collection";
+import { Manager } from '../../Manager/Service/managerPost';
 
 async function getStaffPostsRequest(id) {
     try {
-      const response = await axios.post(`${serverHost}/publisherStaff/pets?staffId=${id}`, { withCredentials: true });
+      let manager = new Manager();
+        manager.managerId = id
+      const response = await axios.post(`${serverHost}/publisherStaff/pets`, manager, { withCredentials: true });
       return response.data;
     } catch (error) {
       console.error('Error Posts :', error);
